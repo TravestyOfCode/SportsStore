138216edcd98cd9a3d40d79d9cb81c224b31a309
travis@travestyofcode.com <travis@travestyofcode.com>
2023-01-10 02:30:55 -0500

    Adds tests.

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using SportsStore.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Controllers;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 
@@ -12,11 +13,11 @@ public class HomeControllerTests
 
         mock.Setup(m => m.Products).Returns((new Product[]
         {
-            new Product { ProductID = 1, Name = "P1" },
-            new Product { ProductID = 2, Name = "P2" },
-            new Product { ProductID = 3, Name = "P3" },
-            new Product { ProductID = 4, Name = "P4" },
-            new Product { ProductID = 5, Name = "P5" }
+            new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+            new Product { ProductID = 2, Name = "P2", Category = "Cat2"  },
+            new Product { ProductID = 3, Name = "P3", Category = "Cat1"  },
+            new Product { ProductID = 4, Name = "P4", Category = "Cat2"  },
+            new Product { ProductID = 5, Name = "P5", Category = "Cat3"  }
         }).AsQueryable<Product>);
 
         return mock;
@@ -31,7 +32,7 @@ public class HomeControllerTests
         HomeController controller = new HomeController(mock.Object);
 
         // Act
-        ProductsListViewModel result = controller.Index()?.ViewData.Model as ProductsListViewModel ?? new();
+        ProductsListViewModel result = controller.Index(null)?.ViewData.Model as ProductsListViewModel ?? new();
 
         // Assert
         Product[] prodArray = result.Products.ToArray();
@@ -52,7 +53,7 @@ public class HomeControllerTests
         controller.PageSize = 3;
 
         // Act
-        ProductsListViewModel result = controller.Index(2)?.ViewData.Model as ProductsListViewModel ?? new();
+        ProductsListViewModel result = controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
 
         // Assert
         Product[] prodArray = result.Products.ToArray();
@@ -70,7 +71,7 @@ public class HomeControllerTests
         HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
 
         // Act
-        ProductsListViewModel result = controller.Index(2)?.ViewData.Model as ProductsListViewModel ?? new();
+        ProductsListViewModel result = controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
 
         // Assert
         PagingInfo pageInfo = result.PagingInfo;
@@ -78,5 +79,47 @@ public class HomeControllerTests
         Assert.Equal(3, pageInfo.ItemsPerPage);
         Assert.Equal(5, pageInfo.TotalItems);
         Assert.Equal(2, pageInfo.TotalPages);
+    }
+
+    [Fact]
+    public void Can_Filter_Products()
+    {
+        // Arrange
+        Mock<IStoreRepository> mock = SetupStoreRepository();
+
+        HomeController controller = new HomeController(mock.Object);
+        controller.PageSize = 3;
+
+        // Action
+        Product[] result = (controller.Index("Cat2", 1)?.ViewData.Model as ProductsListViewModel ?? new()).Products.ToArray();
+
+        // Assert
+        Assert.Equal(2, result.Length);
+        Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+        Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+    }
+
+    [Fact]
+    public void Generate_Category_Specific_Product_Count()
+    {
+        // Arrange
+        Mock<IStoreRepository> mock = SetupStoreRepository();
+
+        HomeController target = new HomeController(mock.Object);
+        target.PageSize = 3;
+
+        Func<ViewResult, ProductsListViewModel?> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+        // Action
+        int? res1 = GetModel(target.Index("Cat1"))?.PagingInfo.TotalItems;
+        int? res2 = GetModel(target.Index("Cat2"))?.PagingInfo.TotalItems;
+        int? res3 = GetModel(target.Index("Cat3"))?.PagingInfo.TotalItems;
+        int? resAll = GetModel(target.Index(null))?.PagingInfo.TotalItems;
+
+        // Assert
+        Assert.Equal(2, res1);
+        Assert.Equal(2, res2);
+        Assert.Equal(1, res3);
+        Assert.Equal(5, resAll);
     }
 }
